4f6e8c9809e37bd60b66f72b714223b585bd20c0
kage <tamada007@hotmail.com>
2017-09-19 13:05:57 +0800

    again again

--- server.js
+++ server.js
@@ -146,8 +146,8 @@ function application(req, res) {
                 }
             }
 
-            //var httplib = option.protocol == 'https:' ? https : http;
-            var httplib = https;//kage
+            var httplib = option.protocol == 'https:' ? https : http;
+            //var httplib = https;//kage
             option.path = option.path ? option.path : option.pathname+(option.search ? option.search : '');
             option.method = request.method;
             option.headers = request.headers;
